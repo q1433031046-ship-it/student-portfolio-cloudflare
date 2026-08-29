03efc8d8d8d0b4a1c5b3551d833fa174e0c3f1b3
西伯利亚大仓鼠 <q1433031046@gmail.com>
2026-08-29 16:35:41 +0800

    Keep development version independent from JSON imports

--- app/lib/program-version.ts
+++ app/lib/program-version.ts
@@ -1,4 +1,2 @@
-import manifest from "../../deployment/template-version.json";
-
-export const PROGRAM_VERSION = manifest.version;
+export const PROGRAM_VERSION = "1.1.0";
 export const RECOMMENDED_INITIAL_ADMIN_CODE = "StudentWeb2026A1";
